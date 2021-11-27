6a4869cb97b940f6d5b05f0826ee67570e38babd
Namtmph17849 <namtmph17849@fpt.edu.vn>
2021-11-27 08:38:54 +0700

    add splash

--- app/src/main/java/manga/readder/UI/Splash.java
+++ app/src/main/java/manga/readder/UI/Splash.java
@@ -1,8 +1,14 @@
 package manga.readder.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import manga.readder.Fragment.FragmentManga;
+import manga.readder.Model.Manga;
 import manga.readder.R;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash extends AppCompatActivity {
 
@@ -10,5 +16,13 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1500);
     }
 }
